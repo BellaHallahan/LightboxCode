940ee3ec53feae68aa1502d0da9b930a1aff9d0e
BellaHallahan <bhallahan403@gmail.com>
2024-03-22 13:51:14 -0400

    update

--- bella_and_kim_sumo/bella_and_kim_sumo.ino
+++ bella_and_kim_sumo/bella_and_kim_sumo.ino
@@ -1,10 +1,27 @@
-//final code
 #define MR1 5
 #define MR2 6
 #define ML1 7
 #define ML2 8
 
+#define FWRD 0
+#define BKWRD 1
+
 int x;
+
+//k
+void drive_robot(int dir){
+  if (dir == FWRD){
+    digitalWrite(MR1, HIGH);
+    digitalWrite(MR2, LOW);
+    digitalWrite(ML1, HIGH);
+    digitalWrite(ML2, LOW);
+  }else{
+    digitalWrite(MR1, LOW);
+    digitalWrite(MR2, HIGH);
+    digitalWrite(ML1, LOW);
+    digitalWrite(ML2, HIGH);
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,32 +34,9 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(MR1, HIGH);
-  digitalWrite(MR2, LOW);
-
-  digitalWrite(ML1, LOW);
-  digitalWrite(ML2, HIGH);
-
-  delay(2000);
-
-  digitalWrite(MR1, LOW);
-  digitalWrite(MR2, HIGH);
-
-  digitalWrite(ML1, HIGH);
-  digitalWrite(ML2, LOW);
-
-  delay(2000);
-
-  x = digitalRead(3);
-  delay(500);
-
-  if (x == 0) {
-    Serial.print("White");
-
-  } else {
-    Serial.print("Black");
-  }
-  Serial.println();
-
-} 
+  drive_robot(BKWRD);
+  
+  x = analogRead(3);
+  Serial.println(x);
+  delay(1000);
+}
