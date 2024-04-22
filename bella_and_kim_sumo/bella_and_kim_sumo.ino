805bcba1ece21dd7fef4bcd7ea78b49d5345eae5
BellaHallahan <bhallahan403@gmail.com>
2024-04-22 12:41:14 -0400

    Update bella_and_kim_sumo.ino

--- bella_and_kim_sumo/bella_and_kim_sumo.ino
+++ bella_and_kim_sumo/bella_and_kim_sumo.ino
@@ -3,9 +3,9 @@
 #define ML1 7
 #define ML2 8
 
-#define FWRD 0
-#define BKWRD 1
-#define RGHT 2
+#define FORWARD 0
+#define BACKWARD 1
+#define RIGHT 2
 #define LEFT 3
 #define WHITE 0
 #define BLACK 1
@@ -13,8 +13,24 @@
 int x;
 int y;
 
+void motor(int m, int d) {
+  if ((m == RIGHT) && (d == FORWARD)) {
+    digitalWrite(MR1, LOW);
+    digitalWrite(MR2, HIGH);
+  } else if ((m == RIGHT) && (d == BACKWARD)) {
+    digitalWrite(MR1, HIGH);
+    digitalWrite(MR2, LOW);
+  } else if ((m == LEFT) && (d == FORWARD)) {
+    digitalWrite(ML1, LOW);
+    digitalWrite(ML2, HIGH);
+  } else if ((m == LEFT) && (d == BACKWARD)) {
+    digitalWrite(ML1, HIGH);
+    digitalWrite(ML2, LOW);
+  }
+}
+
 void drive_robot(int dir) {
-  if (dir == FWRD) {
+  if (dir == FORWARD) {
     digitalWrite(MR1, HIGH);
     digitalWrite(MR2, LOW);
     digitalWrite(ML1, HIGH);
@@ -27,7 +43,7 @@ void drive_robot(int dir) {
   }
 }
 void turn_robot(int dir) {
-  if (dir == RGHT) {
+  if (dir == RIGHT) {
     digitalWrite(MR1, LOW);
     digitalWrite(MR2, HIGH);
     digitalWrite(ML1, HIGH);
@@ -49,26 +65,30 @@ void setup() {
   pinMode(2, INPUT);
   Serial.begin(9600);
 
+  motor(LEFT, BACKWARD);
+  while(1);
+
 }
 
 void loop() {
   int rightSensor = digitalRead(3);
   int leftSensor = digitalRead(2);
-  
-  if(rightSensor == WHITE){
-    drive_robot(BKWRD); 
-    delay(1000);
-    turn_robot(RGHT);
-    delay(1000);
+
+  if (rightSensor == WHITE) {
+    drive_robot(BACKWARD);
+    delay(3000);
+    turn_robot(RIGHT);
+    delay(3000);
     Serial.println("white");
-  }else if (rightSensor == BLACK){
+  } else if (rightSensor == BLACK) {
+    drive_robot(FORWARD);
     Serial.println("black");
   }
-  
-  
-  drive_robot(FWRD);
+
+
+  //drive_robot(FWRD);
   //delay(1000);
- // turn_robot(RGHT);
+  //turn_robot(RGHT);
   //delay(1000);
   //drive_robot(BKWRD);
   delay(30);
